a468d2d082e3be959f804acbbde78154162c63c9
Graham McGregor <haxxor42@gmail.com>
2014-02-22 14:44:05 -0500

    bare bones for local subdomain

--- precomp/__init__.py
+++ precomp/__init__.py
@@ -10,4 +10,26 @@
 
 from werkzeug.exceptions import NotFound
 
+from flask import Flask, url_for, redirect
 
+class SubFlask(Flask):
+    def route(self, rule, *args, **kwargs):
+        if self.debug:
+            subdomain = kwargs.pop('subdomain', None)
+            if subdomain is not None:
+                rule = '/subdomain/{}/{}'.format(subdomain, rule)
+        return super(SubFlask, self).route(rule, *args, **kwargs)
+
+app = SubFlask(__name__)
+app.config.update(dict(
+    DEBUG=True
+))
+
+@app.route("/")
+def home():
+    return "home page"
+
+@app.route("/", subdomain="<sub>")
+def get_service(sub):
+    """routes subdomains to the right service"""
+    return "hello"
