c30d29855648dd4832f5bed39c095bcf59765723
Phil Schleihauf <uniphil@gmail.com>
2014-02-22 12:54:40 -0500

    hello flask

---
+++ precomp/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    precomp
+    ~~~~~~~
+
+    la la la
+
+    :copyright: 2014 Carey Metcalfe, Graham McGregor, Phil Schleihauf
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
